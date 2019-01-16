a19797c46b3ef7ed830896eba528bc4ebd095014
LukasFriesenecker <43822353+LukasFriesenecker@users.noreply.github.com>
2019-01-16 14:43:29 +0100

    aufgabe 10

---
+++ Prototypes/rectangle.js
@@ -0,0 +1,14 @@
+function Rectangle(length, width){
+    this.length = length;
+    this.width = width;
+
+    Rectangle.prototype.getArea = function(){
+        return this.length * this.width;
+    }
+
+    Rectangle.prototype.getCircumference = function(){
+        return 2 * (this.length + this.width);
+    }
+}
+
+module.exports = Rectangle;
